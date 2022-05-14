925efa997681f394f477d7d2908cbbe6cd02bfa0
OzturkVedat <92203253+OzturkVedat@users.noreply.github.com>
2022-05-14 18:06:53 +0300

    iletişim sayfası hazır

---
+++ iletişim klasörü/iletişim.js
@@ -0,0 +1,28 @@
+function validateForm() {
+  let x = document.forms["tekForm"]["ad"].value;
+  if (x == "") {
+    alert("Adınızı girmediniz..");
+    return false;
+  }
+}
+function validateForm() {
+  let y = document.forms["tekForm"]["soyad"].value;
+  if (y == "") {
+    alert("Soyadınızı girmediniz..");
+    return false;
+  }
+}
+function validateForm() {
+  let z = document.forms["tekForm"]["ülke"].value;
+  if (z == "") {
+    alert("Ülke girmediniz..");
+    return false;
+  }
+}
+function validateForm() {
+  let q = document.forms["tekForm"]["konu"].value;
+  if (q == "") {
+    alert("Bir konu belirtmediniz..");
+    return false;
+  }
+}
